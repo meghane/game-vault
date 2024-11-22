f3f34ca18edfce4dd6f9d7deaad66b619ead38ca
meghan t <meghanthomas2022@fau.edu>
2024-11-21 20:43:55 -0500

    initial commit

---
+++ GameVault/GameVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameVaultApp.swift
+//  GameVault
+//
+//  Created by meghan on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GameVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
