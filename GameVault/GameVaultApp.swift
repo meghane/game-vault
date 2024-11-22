806fd629434f9458a501143045b679fb32e2b4c3
meghan t <meghanthomas2022@fau.edu>
2024-11-21 21:09:53 -0500

    updated the gamevaultapp file

--- GameVault/GameVaultApp.swift
+++ GameVault/GameVaultApp.swift
@@ -2,16 +2,36 @@
 //  GameVaultApp.swift
 //  GameVault
 //
-//  Created by meghan on 11/21/24.
+//  Created by meghan on 11/20/24.
 //
 
 import SwiftUI
+import ParseSwift
 
+//connects the managers to the app
 @main
 struct GameVaultApp: App {
+    @StateObject private var authStateManager = AuthenticationStateManager()
+    @StateObject private var favoritesManager = FavoritesManager()  // Add this
+        
+    init() {
+        initializeParse()
+    }
+    //enables the launchscreen to be the first view
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchScreen()
+                .environmentObject(authStateManager)
+                .environmentObject(favoritesManager)
         }
     }
+  //initializes parse
+    private func initializeParse() {
+        ParseSwift.initialize(
+            applicationId: ParseConfig.applicationId,
+            clientKey: ParseConfig.clientKey,
+            serverURL: URL(string: ParseConfig.serverURL)!
+        )
+    }
 }
+
